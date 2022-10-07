74b9c1832739274ce53c7ce21942186d6666c06d
Nxrdo <61379446+Nxrdo@users.noreply.github.com>
2022-10-07 17:58:35 -0500

    Create Utility.lua

---
+++ Utility.lua
@@ -0,0 +1,20 @@
+local utility = {}
+function utility.newfile(name, data)
+	if not isfile(name) then
+		writefile(name, data)
+	else
+		warn("(" .. name ..") already exists, please use 'appendfile(filename.extension, data)'.")
+	end
+	return name
+end
+
+function utility.newfolder(name)
+	if not isfolder(name) then
+		makefolder(name)
+	else
+		warn("Folder already exists.")
+	end
+	return name
+end
+
+return utility
